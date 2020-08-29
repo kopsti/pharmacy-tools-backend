0125ddc0d30f75e5e10bd1e165c19aba4efa2de1
Timos Kopsidas <kopsidas.t@gmail.com>
2020-08-29 13:16:19 +0300

    Update ChangePass.java

--- src/main/java/gr/appleton/ms/pharmacytools/authorization/dto/ChangePass.java
+++ src/main/java/gr/appleton/ms/pharmacytools/authorization/dto/ChangePass.java
@@ -1,30 +1,26 @@
 package gr.appleton.ms.pharmacytools.authorization.dto;
 
-import gr.appleton.ms.pharmacytools.common.constants.ExceptionMessages;
 import lombok.Getter;
 import lombok.ToString;
-
-import javax.validation.constraints.Email;
 
 /**
  * The type Change pass.
  */
 @Getter
 @ToString
-public final class ChangePass extends AuthModel {
+public final class ChangePass {
 
+    private final String pass;
     private final String updatedPass;
 
     /**
      * Instantiates a new Change pass.
      *
-     * @param username    the username
      * @param pass        the pass
      * @param updatedPass the updated pass
      */
-    public ChangePass(@Email(message = ExceptionMessages.E1) final String username,
-                      final String pass, final String updatedPass) {
-        super(username, pass);
+    public ChangePass(final String pass, final String updatedPass) {
+        this.pass = pass;
         this.updatedPass = updatedPass;
     }
 
