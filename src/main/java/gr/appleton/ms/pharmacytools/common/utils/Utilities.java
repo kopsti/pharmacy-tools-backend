75f0fdc5fbd1db383379a13a428f0d79107d548b
Timos Kopsidas <kopsidas.t@gmail.com>
2020-12-02 00:41:52 +0200

    Add "any" filtering property

--- src/main/java/gr/appleton/ms/pharmacytools/common/utils/Utilities.java
+++ src/main/java/gr/appleton/ms/pharmacytools/common/utils/Utilities.java
@@ -7,10 +7,15 @@ import com.querydsl.core.types.dsl.NumberPath;
 import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.core.types.dsl.StringPath;
 import gr.appleton.ms.pharmacytools.common.dto.SearchCriteria;
+import gr.appleton.ms.pharmacytools.models.accounts.dto.BankAccountDao;
 import gr.appleton.ms.pharmacytools.models.accounts.filtering.BankAccountPredicatesBuilder;
+import gr.appleton.ms.pharmacytools.models.customers.dto.CustomerDao;
 import gr.appleton.ms.pharmacytools.models.customers.filtering.CustomerPredicatesBuilder;
+import gr.appleton.ms.pharmacytools.models.notes.dto.NoteDao;
 import gr.appleton.ms.pharmacytools.models.notes.filtering.NotePredicatesBuilder;
+import gr.appleton.ms.pharmacytools.models.orders.dto.OrderDao;
 import gr.appleton.ms.pharmacytools.models.orders.filtering.OrderPredicatesBuilder;
+import gr.appleton.ms.pharmacytools.models.suppliers.dto.SupplierDao;
 import gr.appleton.ms.pharmacytools.models.suppliers.filtering.SupplierPredicatesBuilder;
 
 import java.util.Calendar;
@@ -100,12 +105,77 @@ public final class Utilities {
      * @return the boolean expression
      */
     public static <T> BooleanExpression handlePredicates(final PathBuilder<T> path, final SearchCriteria criteria) {
+        if ("any".equalsIgnoreCase(criteria.getKey())) {
+            return handleAny(path, criteria);
+        }
         if (isNumeric(criteria.getValue().toString())) {
             return handleNumeric(path, criteria);
         } else if ("true".equals(criteria.getValue()) || "false".equals(criteria.getValue())) {
             return handleBoolean(path, criteria);
         } else {
             return handleString(path, criteria);
+        }
+    }
+
+    private static <T> BooleanExpression handleAny(final PathBuilder<T> entityPath, final SearchCriteria criteria) {
+        if (criteria.getOperation().equalsIgnoreCase(":")) {
+            if (entityPath.getType() == CustomerDao.class) {
+                final StringPath firstName = entityPath.getString("qFirstName");
+                final StringPath lastName = entityPath.getString("qLastName");
+                final StringPath address = entityPath.getString("qAddress");
+                final StringPath homePhoneNumber = entityPath.getString("homePhoneNumber");
+                final StringPath mobilePhoneNumber = entityPath.getString("mobilePhoneNumber");
+                final StringPath email = entityPath.getString("email");
+                final StringPath comments = entityPath.getString("qComments");
+
+                return firstName.containsIgnoreCase(GreekLatin.greek2latin(criteria.getValue().toString()))
+                    .or(lastName.containsIgnoreCase(GreekLatin.greek2latin(criteria.getValue().toString())))
+                    .or(address.containsIgnoreCase(GreekLatin.greek2latin(criteria.getValue().toString())))
+                    .or(homePhoneNumber.containsIgnoreCase(GreekLatin.greek2latin(criteria.getValue().toString())))
+                    .or(mobilePhoneNumber.containsIgnoreCase(GreekLatin.greek2latin(criteria.getValue().toString())))
+                    .or(email.containsIgnoreCase(GreekLatin.greek2latin(criteria.getValue().toString())))
+                    .or(comments.containsIgnoreCase(GreekLatin.greek2latin(criteria.getValue().toString())));
+            } else if (entityPath.getType() == NoteDao.class) {
+                final StringPath content = entityPath.getString("qContent");
+                final StringPath comments = entityPath.getString("qComments");
+
+                return content.containsIgnoreCase(GreekLatin.greek2latin(criteria.getValue().toString()))
+                    .or(comments.containsIgnoreCase(GreekLatin.greek2latin(criteria.getValue().toString())));
+            } else if (entityPath.getType() == BankAccountDao.class) {
+                final StringPath number = entityPath.getString("number");
+                final StringPath iban = entityPath.getString("iban");
+                final StringPath comments = entityPath.getString("qComments");
+
+                return number.containsIgnoreCase(GreekLatin.greek2latin(criteria.getValue().toString()))
+                    .or(iban.containsIgnoreCase(GreekLatin.greek2latin(criteria.getValue().toString())))
+                    .or(comments.containsIgnoreCase(GreekLatin.greek2latin(criteria.getValue().toString())));
+            } else if (entityPath.getType() == SupplierDao.class) {
+                final StringPath title = entityPath.getString("qTitle");
+                final StringPath description = entityPath.getString("qDescription");
+                final StringPath email = entityPath.getString("email");
+                final StringPath phoneNumber = entityPath.getString("phoneNumber");
+                final StringPath taxId = entityPath.getString("taxId");
+                final StringPath taxAuthority = entityPath.getString("taxAuthority");
+                final StringPath comments = entityPath.getString("qComments");
+
+                return title.containsIgnoreCase(GreekLatin.greek2latin(criteria.getValue().toString()))
+                    .or(description.containsIgnoreCase(GreekLatin.greek2latin(criteria.getValue().toString())))
+                    .or(email.containsIgnoreCase(GreekLatin.greek2latin(criteria.getValue().toString())))
+                    .or(phoneNumber.containsIgnoreCase(GreekLatin.greek2latin(criteria.getValue().toString())))
+                    .or(taxId.containsIgnoreCase(GreekLatin.greek2latin(criteria.getValue().toString())))
+                    .or(taxAuthority.containsIgnoreCase(GreekLatin.greek2latin(criteria.getValue().toString())))
+                    .or(comments.containsIgnoreCase(GreekLatin.greek2latin(criteria.getValue().toString())));
+            } else if (entityPath.getType() == OrderDao.class) {
+                final StringPath product = entityPath.getString("qProduct");
+                final StringPath comments = entityPath.getString("qComments");
+
+                return product.containsIgnoreCase(GreekLatin.greek2latin(criteria.getValue().toString()))
+                    .or(comments.containsIgnoreCase(GreekLatin.greek2latin(criteria.getValue().toString())));
+            } else {
+                return null;
+            }
+        } else {
+            return null;
         }
     }
 
