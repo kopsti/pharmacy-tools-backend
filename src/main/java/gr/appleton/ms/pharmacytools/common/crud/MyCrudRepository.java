57744118c3a61232d42059a3101b5e0c92f3666d
Timos Kopsidas <kopsidas.t@gmail.com>
2020-12-02 18:00:27 +0200

    Update MyCrudRepository.java

--- src/main/java/gr/appleton/ms/pharmacytools/common/crud/MyCrudRepository.java
+++ src/main/java/gr/appleton/ms/pharmacytools/common/crud/MyCrudRepository.java
@@ -2,9 +2,23 @@ package gr.appleton.ms.pharmacytools.common.crud;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+/**
+ * The interface My crud repository.
+ *
+ * @param <D> the type parameter
+ * @param <L> the type parameter
+ */
+@Repository
 public interface MyCrudRepository<D, L> extends CrudRepository<D, L> {
 
+    /**
+     * Find by wild card iterable.
+     *
+     * @param wildcard the wildcard
+     * @return the iterable
+     */
     Iterable<D> findByWildCard(@Param("wildcard") String wildcard);
 
 }
